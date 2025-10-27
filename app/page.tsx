1606787bd68c8b06a90d58a578fa39b1dabeedc1
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-27 17:52:10 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home(){
+  return(
+    <>
+    <div className="h-screen bg-gray-300 flex items-center justify-center">
+      <h1 className="text-7xl text-black font-bold">Hello, World</h1>
+    </div>
+    </>
+  )
+}
